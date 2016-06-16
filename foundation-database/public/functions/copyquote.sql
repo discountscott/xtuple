6bfa54c9596f3a971728f7d32b18ba11cd54c2f8
Scott D Moore <scott@discountfence.com>
2016-06-16 09:18:50 -0400

    Issue 28080
    If metric "WholesalePriceCosting" is true, pull the current listcost.
    Otherwise fall back to standard costing.

--- foundation-database/public/functions/copyquote.sql
+++ foundation-database/public/functions/copyquote.sql
@@ -56,7 +56,8 @@ BEGIN
   INSERT INTO quitem
   ( quitem_quhead_id, quitem_linenumber, quitem_itemsite_id,
     quitem_scheddate, quitem_promdate, quitem_qtyord,
-    quitem_price, quitem_custprice, quitem_unitcost,
+    quitem_price, quitem_custprice,
+	CASE WHEN (fetchMetricText('WholesalePriceCosting') = 't') THEN (select (item_listcost) from item where item_id=quitem_item_id) ELSE stdCost(itemsite_item_id) END,
     quitem_qty_uom_id, quitem_price_uom_id,
     quitem_qty_invuomratio, quitem_price_invuomratio,
     quitem_memo, quitem_custpn, quitem_imported, quitem_taxtype_id,
