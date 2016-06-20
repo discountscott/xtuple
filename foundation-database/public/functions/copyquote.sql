543aa68d7e487fb0e0e272ceba781d116dd1f408
Scott D Moore <scott@discountfence.com>
2016-06-20 14:40:45 -0400

    Fix Typo

--- foundation-database/public/functions/copyquote.sql
+++ foundation-database/public/functions/copyquote.sql
@@ -56,8 +56,7 @@ BEGIN
   INSERT INTO quitem
   ( quitem_quhead_id, quitem_linenumber, quitem_itemsite_id,
     quitem_scheddate, quitem_promdate, quitem_qtyord,
-    quitem_price, quitem_custprice,
-    CASE WHEN (fetchMetricText('WholesalePriceCosting') = 't') THEN (select (item_listcost) from item where item_id=quitem_item_id) ELSE stdCost(itemsite_item_id) END,
+    quitem_price, quitem_custprice,quitem_unitcost,
     quitem_qty_uom_id, quitem_price_uom_id,
     quitem_qty_invuomratio, quitem_price_invuomratio,
     quitem_memo, quitem_custpn, quitem_imported, quitem_taxtype_id,
@@ -67,7 +66,7 @@ BEGIN
          COALESCE(pSchedDate, quitem_scheddate),
          quitem_promdate,
          quitem_qtyord,
-         quitem_price, quitem_custprice, stdCost(itemsite_item_id),
+         quitem_price, quitem_custprice,
          quitem_qty_uom_id, quitem_price_uom_id,
          quitem_qty_invuomratio, quitem_price_invuomratio,
          quitem_memo, quitem_custpn, FALSE, quitem_taxtype_id,
